3f4390a29f97a1c7912c161bd63c6e073812e00b
jmccarthy92 <jmccarthyns92@gmail.com>
2018-07-19 13:28:55 +0000

    Add stripe payment for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :payment
+  #  when we sign a up a user it's handled through the registration new form
+  # so in that form not only are we going to hit theu sers but we will hit the
+  # payments aswell
+  accepts_nested_attributes_for :payment
 end
